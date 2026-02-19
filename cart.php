5de0fc398c411fadc73a151b8b0c82e6f9142c6f
mohitma2o <mohitmattoo05@gmail.com>
2026-02-19 16:00:42 +0530

    Developer B: Added cart discount message

--- cart.php
+++ cart.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <title>OG Tech PC - Cart</title>
 <?php include "header.php"; ?>
+<?php echo "Special Cart Discount Active"; // Developer B feature ?>
 
 <div class="wide-container">
   <?php include "includes/order.inc.php" ?>
