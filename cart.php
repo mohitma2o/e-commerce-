1db8aa1a155b046e74a6159a3ca0e618811c7953
mohitma2o <mohitmattoo05@gmail.com>
2026-02-20 08:32:13 +0530

    SmartMerge AI resolved conflict

--- cart.php
+++ cart.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <html lang="en">
 <title>OG Tech PC - Cart</title>
 <?php include "header.php"; ?>
@@ -6,9 +5,3 @@
 
 <div class="wide-container">
   <?php include "includes/order.inc.php" ?>
-  <?php include "static/pages/cart_items.php" ?>
-  <?php include "static/pages/order_items.php" ?>
-</div>
-
-<?php include "footer.php"; ?>
-</html>
