015f8f228d0976789e55052d4dedfa1f7975d919
mohitma2o <mohitmattoo05@gmail.com>
2026-02-20 05:44:06 +0530

    SmartMerge AI resolved conflict

--- manage_profile.php
+++ manage_profile.php
@@ -10,175 +10,76 @@ echo "<h4 style='text-align:center;'>Welcome back, $username!</h4>";
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>OG Tech - My Profile Dashboard</title>
-<meta name="theme-color" content="#ff9800">
-
+  <meta name="theme-color" content="#ff9800">
   <?php include "header.php"; ?>
 </head>
 <body>
-<h3 class="page-title grid" style="margin-top: 50px;">Manage Personal Profile</h3>
-  <div class="grid">
 
+  <div class="errormsg">
+    <?php
+    if (isset($_GET["error"])) {
+      echo "<script>
+        let msg = document.getElementById('msg');
+        msg.className = 'red-text bold pulse';
+        msg.innerHTML = 'Please review your information carefully!';
+      </script>";
+    }
 
-    <div class="rounded-card-parent black">
-      <div class="card rounded-card black" style="width: 650px; ">
-        <div class="row">
-          <button id="edit" style="text-align:left" class="btn orange " onclick="confirm_edit(this)" style="margin-right: 20px">Edit</button>
-          <div class="errormsg bold"><p id="msg" class="red-text"></p></div>
+    if ($_GET["error"] == "empty_input")
+      echo "<script>document.getElementById('msg').innerHTML = '*Fill in all fields!';</script>";
 
-          <div class="errormsg">
-            <?php
-              <?php
-if (isset($_GET["error"])) {
-  echo "<script>
-    let msg = document.getElementById('msg');
-    msg.className = 'red-text bold pulse';
-    msg.innerHTML = 'Please review your information carefully!';
-  </script>";
-}
-?>
-
-                if ($_GET["error"] == "empty_input")
-                  echo "<script>document.getElementById('msg').innerHTML = '*Fill in all fields!';</script>";
-
-                else if ($_GET["error"] == "invalid_uid")
-                  echo "<script>document.getElementById('msg').innerHTML = '*Choose a proper username!';</script>";
-
-                else if ($_GET["error"] == "passwords_dont_match")
-                  echo "<script>document.getElementById('msg').innerHTML = '*Passwords doesn't match!';</script>";
-
-                else if ($_GET["error"] == "stmtfailed")
-                  echo "<script>document.getElementById('msg').innerHTML = '*Something went wrong, please try again!';</script>";
-
-                else if ($_GET["error"] == "username_taken")
-                  echo "<script>document.getElementById('msg').innerHTML = '*Username already taken!';</script>";
-
-                else if ($_GET["error"] == "none")
-                {
-                  echo "<script>document.getElementById('msg').className = 'green-text';</script>";
-                  echo "<script>document.getElementById('msg').innerHTML = 'Profile updated!';</script>";
-                }
-                M.toast({html: 'Profile updated successfully!'});
-
-              }
-
-            ?>
-          </div>
-        </div>
-        <div class="card-content grey darken-4 white-text">
-          <form class="s12" action="includes/manage_profile.inc.php" method="POST">
-          <input type="file" name="profile_pic" accept="image/*">
-
-            <div class="row">
-              <div class="input-field s6">
-                <i class="material-icons prefix">account_circle</i>
-                <?php
-                echo "<input disabled name='id' type='hidden' value='$memberID'/>";
-                echo"<input disabled class='validate white-text'
-      maxlength='20'
-      required
-      name='username'
-      id='username'
-      type='text'
-      value='$username'/>";
-
-                ?>
-                <label class='cyan-text' for="username">Enter New Username</label>
-                <span class="helper-text grey-text" data-error="Min 5, Max 12 characters" data-success="Min 5, Max 12 characters">Min 5, Max 12 characters</span>
-              </div>
-            </div>
-            <div class="row">
-              <div class="input-field s6">
-                <i class="material-icons prefix">email</i>
-                <?php
-                echo "<input disabled class='white-text validate' name='email' id='email' type='email' value='$email'/>";
-                ?>
-                <label class='cyan-text' for="email">Enter New Email</label>
-                <span class="helper-text white-text" data-error="wrong" data-success="correct"></span>
-              </div>
-            </div>
-            <div class="row">
-              <div class="input-field s6">
-                <i class="material-icons prefix"> password</i>
-                <input disabled class='white-text validate' name="pwd" id="pwd" type="password" minlength="8" maxlength="20">
-                <label class='cyan-text' for="pwd">Enter New Password</label>
-                <span class="helper-text grey-text" data-error="Min 8, Max 20 characters" data-success="Min 8, Max 20 characters">Min 8, Max 20 characters</span>
-              </div>
-            </div>
-            <div class="row">
-              <div class="input-field s6">
-                <i class="material-icons prefix"> password</i>
-                <input disabled class='white-text validate' name="repeat_pwd" id="repeat_pwd" type="password" maxlength="20">
-                <label class='cyan-text' for="repeat_pwd"> Repeat New Password</label>
-              </div>
-            </div>
-          <br>
-          <p class="center-align">
-          <button disabled id="update_acc"
-class="btn green lighten-1"
-
-          </p>
-          </form>
-        </div>        
-      </div>
-    </div>
+    if ($_GET["error"] == "none") {
+      echo "<script>document.getElementById('msg').className = 'green-text';</script>";
+      echo "<script>document.getElementById('msg').innerHTML = 'Profile updated!';</script>";
+      M.toast({html: 'Profile updated successfully!'});
+    }
+    ?>
   </div>
+  
+  <div class="card-content grey darken-4 white-text">
+    <form class="s12" action="includes/manage_profile.inc.php" method="POST">
+      <input type="file" name="profile_pic" accept="image/*">
+      <div class="row">
+        <div class="input-field s6">
+          <i class="material-icons prefix">account_circle</i>
+          <?php
+          echo "<input disabled name='id' type='hidden' value='$memberID'/>";
+          echo "<input disabled class='validate white-text' maxlength='20' required name='username' id='username' type='text' value='$username'/>";
+          ?>
+          <label class='cyan-text' for="username">Enter New Username</label>
+          <span class="helper-text grey-text" data-error="Min 5, Max 12 characters" data-success="Min 5, Max 12 characters">Min 5, Max 12 characters</span>
+        </div>
+      </div>
+      <br>
+      <p class="center-align">
+        <button disabled id="update_acc" class="btn green lighten-1" type="submit" name="update">Update Account</button>
+      </p>
+    </form>
+  </div>        
+
+  <script>
+    var submitBtn = document.querySelector("#update_acc");
+
+    function confirm_edit(btn) {
+      username.classList.toggle("highlight");
+      email.classList.toggle("highlight");
+
+      username.disabled = false;
+      email.disabled = false;
+      pwd.disabled = false;
+      repeatPwd.disabled = false;
+      submitBtn.disabled = false;
+
+      btn.textContent = "Editing Enabled";
+    }
+
+    function fade_in() {
+      $("#msg").fadeIn().delay(2500).fadeOut();
+    }
+
+    function togglePwd() {
+      pwd.type = pwd.type === "password" ? "text" : "password";
+    }
+  </script>
 </body>
-<?php include "footer.php"; ?>
-
-<script>
-  // disable and enable input fields
-  var id =  document.getElementsByName("id")[0];
-  var username =  document.getElementsByName("username")[0];
-  var email =  document.getElementsByName("email")[0];
-  var pwd =  document.getElementsByName("pwd")[0];
-  var repeatPwd =  document.getElementsByName("repeat_pwd")[0];
-  var submitBtn = document.querySelector("#update_acc");
-
-function confirm_edit(btn)
-{
-  username.classList.toggle("highlight");
-  email.classList.toggle("highlight");
-
-  username.disabled = false;
-  email.disabled = false;
-  pwd.disabled = false;
-  repeatPwd.disabled = false;
-  submitBtn.disabled = false;
-
-  btn.textContent = "Editing Enabled";
-}
-
-{
-  id.disabled = !id.disabled;
-
-  if (id.disabled)
-  {
-    username.disabled = true;
-    email.disabled = true;
-    pwd.disabled = true;
-    repeatPwd.disabled = true;
-    submitBtn.disabled = true;
-    btn.textContent = "Edit"
-  } else
-  {
-    username.disabled = false;
-    email.disabled = false;
-    pwd.disabled = false;
-    repeatPwd.disabled = false;
-    submitBtn.disabled = false;
-    btn.textContent = "Done"
-  }
-}
-
-// timed message 
-setTimeout(fade_in, 2500);
-
-function fade_in() {
-  $("#msg").fadeIn().delay(2500).fadeOut();
-}
-function togglePwd() {
-  pwd.type = pwd.type === "password" ? "text" : "password";
-}
-
-</script>
 </html>
